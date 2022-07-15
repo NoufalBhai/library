c1e5cfc6731c978ab5de688faa8ddf250f37625c
Noufal Bhai <noufalbhai666@gmail.com>
2022-07-15 22:21:06 +0530

    Refactoring Users Code

---
+++ app/crud/users.py
@@ -0,0 +1,80 @@
+from fastapi import status
+from fastapi.responses import JSONResponse
+from passlib.context import CryptContext
+from psycopg2.extras import DictCursor
+from psycopg2.errors import UniqueViolation
+
+
+
+from app.db import conn
+from app.schema import RegisterUser, BaseUser
+
+context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def create(user: RegisterUser):
+    query = """
+    INSERT INTO library.users(name, phone, email, password)
+    VALUES (%s, %s, %s, %s) RETURNING *;
+    """
+    hashed_pwd = context.hash(user.password)
+    params = (user.name, user.phone, user.email, hashed_pwd)
+    try:
+        cursor = conn.cursor(cursor_factory=DictCursor)
+        cursor.execute(query, params)
+        conn.commit()
+        new_user = cursor.fetchone()
+    except UniqueViolation as uv:
+        conn.rollback()
+        if "users_email_key" in str(uv):
+            message = "Email is already Exist"
+        if "users_phone_key" in str(uv):
+            message = "Phone number is already Exist"
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST, content={"message": message}
+        )
+    finally:
+        cursor.close()
+    return dict(new_user)
+
+def get_all(offset: int, limit: int):
+    query = "SELECT * FROM library.users OFFSET %s LIMIT %s;"
+    params = (offset, limit)
+    cursor = conn.cursor(cursor_factory=DictCursor)
+    cursor.execute(query, params)
+    all_users = cursor.fetchall()
+    cursor.close()
+    return [dict(user) for user in all_users]
+
+def get_one(id: int):
+    query = "SELECT * FROM library.users WHERE id = %s"
+    params = (id,)
+    cursor = conn.cursor(cursor_factory=DictCursor)
+    cursor.execute(query, params)
+    user = cursor.fetchone()
+    cursor.close()
+    return user
+
+def delete(id: int):
+    query = "DELETE FROM library.users WHERE id = %s"
+    params = (id,)
+    cursor = conn.cursor(cursor_factory=DictCursor)
+    cursor.execute(query, params)
+    conn.commit()
+    cursor.close()
+    
+def update(user: BaseUser):
+    query = """
+    UPDATE library.users 
+    SET name=%s, 
+        email=%s, 
+        phone=%s 
+    WHERE id=%s RETURNING *;
+    """
+    params = (user.name, user.email, user.phone, id)
+    cursor = conn.cursor(cursor_factory=DictCursor)
+    cursor.execute(query, params)
+    changed_user_in_db = cursor.fetchone()
+    conn.commit()
+    cursor.close()
+    return changed_user_in_db
--- app/users.py
+++ app/users.py
@@ -1,17 +1,10 @@
-from fastapi import APIRouter, HTTPException, Response, status
+from fastapi import APIRouter, Response, status
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel, EmailStr
-from psycopg2.extras import DictCursor
-from psycopg2.errors import UniqueViolation
 
-from passlib.context import CryptContext
-
+from app.crud import users
 from app import schema
-from app.db import conn
 
 router = APIRouter()
-
-context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 @router.post("/", response_model=schema.ReturnUser)
@@ -19,109 +12,48 @@ def add_user(user: schema.RegisterUser):
     """
     This Function Will Create a new user
     """
-    query = """
-    INSERT INTO library.users(name, phone, email, password)
-    VALUES (%s, %s, %s, %s) RETURNING *;
-    """
-    hashed_pwd = context.hash(user.password)
-    params = (user.name, user.phone, user.email, hashed_pwd)
-    try:
-        cursor = conn.cursor(cursor_factory=DictCursor)
-        cursor.execute(query, params)
-        conn.commit()
-        new_user = cursor.fetchone()
-    except UniqueViolation as uv:
-        conn.rollback()
-        if "users_email_key" in str(uv):
-            message = "Email is already Exist"
-        if "users_phone_key" in str(uv):
-            message = "Phone number is already Exist"
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST, content={"message": message}
-        )
-    finally:
-        cursor.close()
-    return dict(new_user)
+    return users.create(user)
 
 
 @router.get("/", response_model=list[schema.ReturnUser])
 def get_all_users(limit: int = 3, offset: int = 0):
-    query = "SELECT * FROM library.users OFFSET %s LIMIT %s;"
-    params = (offset, limit)
-    cursor = conn.cursor(cursor_factory=DictCursor)
-    cursor.execute(query, params)
-    all_users = cursor.fetchall()
-    cursor.close()
-    # au = []
-    # for user in all_users:
-    #     au.append(dict(user))
-    return [dict(user) for user in all_users]
+    return users.get_all(offset, limit)
 
 
 @router.get(
     "/{id}",
     response_model=schema.ReturnUser,
-    responses={200: {"model": schema.RegisterUser}, 404: {"model": dict[str, str]}},
+    responses={
+        200: {"model": schema.RegisterUser},
+        404: {"model": dict[str, str]},
+    },
 )
 def get_user(id: int):
-    query = "SELECT * FROM library.users WHERE id = %s"
-    params = (id,)
-    cursor = conn.cursor(cursor_factory=DictCursor)
-    cursor.execute(query, params)
-    user = cursor.fetchone()
-    cursor.close()
+    user = users.get_one(id)
     if not user:
         return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND, content={"message": "No User Found"}
         )
-
     return dict(user)
+
 
 @router.delete("/{id}", status_code=204, response_class=Response)
 def delete_user(id: int):
-    query = "SELECT * FROM library.users WHERE id = %s"
-    params = (id,)
-    cursor = conn.cursor(cursor_factory=DictCursor)
-    cursor.execute(query, params)
-    user = cursor.fetchone()
-    cursor.close()
+    user = users.get_one(id)
     if not user:
         return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND, content={"message": "No User Found"}
         )
-    
-    query = "DELETE FROM library.users WHERE id = %s"
-    params = (id,)
-    cursor = conn.cursor(cursor_factory=DictCursor)
-    cursor.execute(query, params)
-    conn.commit()
-    cursor.close()
+    users.delete(id)
     return None
+
 
 @router.put("/{id}", response_model=schema.ReturnUser)
 def update_user(id: int, user: schema.BaseUser):
-    query = "SELECT * FROM library.users WHERE id = %s"
-    params = (id,)
-    cursor = conn.cursor(cursor_factory=DictCursor)
-    cursor.execute(query, params)
-    user_in_db = cursor.fetchone()
+    user_in_db = users.get_one(id)
     if not user_in_db:
         return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND, content={"message": "No User Found"}
         )
-
-    query = """
-    UPDATE library.users 
-    SET name=%s, 
-        email=%s, 
-        phone=%s 
-    WHERE id=%s RETURNING *;
-    """
-    params = (user.name, user.email, user.phone, id)
-    
-    cursor.execute(query, params)
-    changed_user_in_db = cursor.fetchone()
-    conn.commit()
-    cursor.close()
+    changed_user_in_db = users.update(user)
     return dict(changed_user_in_db)
-
