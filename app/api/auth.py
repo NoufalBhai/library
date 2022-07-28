892e132c9a65aeaa39337bce83790d80b668013d
Noufal Bhai <noufalbhai666@gmail.com>
2022-07-28 21:07:21 +0530

    Adding More Authentication with JWT

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, Depends, Request, status, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from passlib.context import CryptContext
 
 from app.schemas import users
 from app.crud import users as cu_user
+from app.utils.auth import generate_jwt, parse_jwt
 
 router = APIRouter()
 
@@ -21,7 +23,20 @@ def login(user: users.Login):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Email or Password Missmatch"
         )
-    
+    token = generate_jwt(user_in_db.get("id"))
     return {
-        "message": "User Login Success"
+        "message": "User Login Success",
+        "access_token": token
     }
+
+def get_user_id(token: HTTPAuthorizationCredentials=Depends(HTTPBearer())) -> str:
+    return parse_jwt(token.credentials)
+
+@router.get("/only-for-logged-in")
+def secret_data(user_id: int = Depends(get_user_id)):
+    return {
+        "message": "It's only for logged in Users",
+        "data": "This is a secret",
+        "user_id": user_id 
+    }
+
--- app/db.py
+++ app/db.py
@@ -1,5 +1,10 @@
 import psycopg2
+from app.utils.config import settings
 
 conn = psycopg2.connect(
-    host="localhost", port=5432, user="postgres", password="password", dbname="library"
+    host=settings.DB_HOST,
+    port=settings.DB_PORT,
+    user=settings.DB_USER,
+    password=settings.DB_PWD,
+    dbname=settings.DB_NAME
 )
